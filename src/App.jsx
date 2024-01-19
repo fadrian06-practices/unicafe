3a71e04a39ced6ddc8274eec02b307a89f107cf0
fadrian06 <franyeradriansanchez@gmail.com>
2024-01-19 16:38:53 -0400

    1.8: unicafe, step 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,11 @@
 import { useState } from 'react'
 
-const App = () => {
-  // guarda los clics de cada botón en su propio estado
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
+// un lugar adecuado para definir un componente
+const Statistics = ({ good, neutral, bad }) => {
   const all = good + neutral + bad
 
   return (
     <>
-      <h2>Give feedback</h2>
-      <button onClick={() => setGood(good + 1)}>Good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
-      <button onClick={() => setBad(bad + 1)}>Bad</button>
       <h2>Statistics</h2>
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
@@ -25,6 +17,23 @@ const App = () => {
           <p>Positive {(good / all) * 100} %</p>
         </>
       )}
+    </>
+  )
+}
+
+const App = () => {
+  // guarda los clics de cada botón en su propio estado
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <>
+      <h2>Give feedback</h2>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </>
   )
 }
